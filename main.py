7c74a3e244f4372a5effb46d19ed0c89a7a29251
BirPechkin <gud2084@gmail.com>
2026-07-07 23:02:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,14 @@
-import os
 import requests
-from bs4 import BeautifulSoup
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
+url = "https://rabotavdodo.ru/api/dodois/get-pizzeria-by-vacancy?localities=ca2ee3d72c08bfa111efbd700111a8ac&locale=ru&staffTypes=Courier"
 
-URL = "https://rabotavdodo.ru/Zaraysk?tabSlug=delivery"
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://rabotavdodo.ru/Zaraysk?tabSlug=delivery",
+    "Accept": "*/*"
+}
 
-KEYWORDS = [
-    "курьер",
-    "водитель",
-    "автомобил",
-    "личн"
-]
+response = requests.get(url, headers=headers)
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {
-        "chat_id": CHAT_ID,
-        "text": text
-    }
-    requests.post(url, data=data)
-
-def check_vacancy():
-    response = requests.get(URL)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    page_text = soup.get_text(" ").lower()
-
-    found = []
-
-    for word in KEYWORDS:
-        if word in page_text:
-            found.append(word)
-
-    if found:
-        send_message(
-            "🍕 Додо Зарайск\n"
-            "Нашёл признаки вакансии!\n\n"
-            "Найдено: " + ", ".join(found) +
-            "\n\n" + URL
-        )
-    else:
-        send_message(
-            " Что видит бот на странице:\n\n"
-            + page_text[:500]
-        )
-
-check_vacancy()
+print(response.status_code)
+print(response.text[:1000])

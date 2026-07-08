d39ecefd10b5839a36c5d4467d0b18f5dc450934
BirPechkin <gud2084@gmail.com>
2026-07-08 13:35:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,24 @@
 from playwright.sync_api import sync_playwright
-import json
+import requests
 import time
-
 
 URL = "https://rabotavdodo.ru/Lukhovitsy/eec89bd2a31db46311eedc5e2260fa4f/4254ceb0edc5826d11eff2ad492b901d?tabSlug=all#calculator"
 
+with sync_playwright() as p:
 
-def main():
-    with sync_playwright() as p:
+    browser = p.chromium.launch(
+        headless=True
+    )
 
-        browser = p.chromium.launch(
-            headless=True,
-            args=[
-                "--no-sandbox",
-                "--disable-dev-shm-usage"
-            ]
-        )
+    page = browser.new_page()
 
-        page = browser.new_page()
+    page.goto(URL, wait_until="networkidle", timeout=60000)
 
-        print("OPEN PAGE")
-        page.goto(URL, wait_until="networkidle", timeout=60000)
+    time.sleep(5)
 
-        time.sleep(5)
+    html = page.content()
 
-        print("TITLE:")
-        print(page.title())
+    print("PAGE LOADED")
+    print(html[:1000])
 
-        print("URL:")
-        print(page.url)
-
-        text = page.locator("body").inner_text()
-
-        print("PAGE TEXT:")
-        print(text[:2000])
-
-        browser.close()
-
-
-if __name__ == "__main__":
-    main()
+    browser.close()

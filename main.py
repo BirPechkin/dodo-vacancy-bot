ba44bb45107607bd5e6050be4b49b8d24494d453
BirPechkin <gud2084@gmail.com>
2026-07-08 12:41:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from playwright.sync_api import sync_playwright
+from playwright.__main__ import main as playwright_main
 import requests
+import os
+
+# Скачать браузер, если его нет
+os.system("playwright install chromium")
 
 URL = "https://rabotavdodo.ru/api/dodois/vacancies?localities=eec89bd2a31db46311eedc5e2260fa4f&staffTypes=Courier"
 
@@ -7,9 +12,7 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
 
-    # Заходим на API через браузер, чтобы пройти защиту
     response = page.goto(URL, wait_until="networkidle")
-
     print("FIRST STATUS:", response.status)
 
     cookies = page.context.cookies()
@@ -20,13 +23,10 @@ session = requests.Session()
 for cookie in cookies:
     session.cookies.set(cookie["name"], cookie["value"])
 
-headers = {
+r = session.get(URL, headers={
     "User-Agent": "Mozilla/5.0",
     "Referer": "https://rabotavdodo.ru/"
-}
-
-r = session.get(URL, headers=headers)
+})
 
 print("SECOND STATUS:", r.status_code)
-print("ANSWER:")
 print(r.text)

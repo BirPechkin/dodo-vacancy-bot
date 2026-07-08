d82a0605e2f1222e252bd53eccc474273e2afadb
BirPechkin <gud2084@gmail.com>
2026-07-08 12:32:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,32 @@
-import os
-import subprocess
-import time
 from playwright.sync_api import sync_playwright
+import requests
 
-os.environ["PLAYWRIGHT_BROWSERS_PATH"] = "0"
-
-# Для первого запуска скачиваем Chromium
-subprocess.run(
-    ["python", "-m", "playwright", "install", "chromium"],
-    check=True
-)
-
-url = "https://rabotavdodo.ru/api/dodois/vacancies?localities=ca2ee3d72c08bfa111efbd700111a8ac&staffTypes=Courier&locale=ru"
+URL = "https://rabotavdodo.ru/api/dodois/vacancies?localities=eec89bd2a31db46311eedc5e2260fa4f&staffTypes=Courier"
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(
-        headless=True
-    )
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
 
-    context = browser.new_context(
-        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/146 Safari/537.36",
-        locale="ru-RU"
-    )
-
-    page = context.new_page()
-
-    # Первый заход — пройти защиту ServicePipe
-    response = page.goto(
-        url,
-        wait_until="domcontentloaded",
-        timeout=60000
-    )
+    # Заходим на API через браузер, чтобы пройти защиту
+    response = page.goto(URL, wait_until="networkidle")
 
     print("FIRST STATUS:", response.status)
 
-    # ждём выдачу cookies
-    time.sleep(10)
-
-    print("COOKIES:")
-    print(context.cookies())
-
-    # Второй запрос после прохождения защиты
-    response = page.goto(
-        url,
-        wait_until="networkidle",
-        timeout=60000
-    )
-
-    print("SECOND STATUS:", response.status)
-
-    print("ANSWER:")
-    print(page.text_content("body"))
-
+    cookies = page.context.cookies()
     browser.close()
+
+session = requests.Session()
+
+for cookie in cookies:
+    session.cookies.set(cookie["name"], cookie["value"])
+
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Referer": "https://rabotavdodo.ru/"
+}
+
+r = session.get(URL, headers=headers)
+
+print("SECOND STATUS:", r.status_code)
+print("ANSWER:")
+print(r.text)

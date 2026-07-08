8707d82677a4632d80f247881f3f795e8405f186
BirPechkin <gud2084@gmail.com>
2026-07-08 13:45:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,72 @@
 from playwright.sync_api import sync_playwright
-import os
+import requests
 import time
 
 
-URL = "https://rabotavdodo.ru/Lukhovitsy/eec89bd2a31db46311eedc5e2260fa4f/4254ceb0edc5826d11eff2ad492b901d?tabSlug=all#calculator"
+URL = "https://rabotavdodo.ru/api/dodois/vacancies?localities=ca2ee3d72c08bfa111efbd700111a8ac&staffTypes=Courier"
 
 
 def main():
-    # путь, куда Render ставит браузер при PLAYWRIGHT_BROWSERS_PATH=0
-    browser_path = "/opt/render/project/src/.venv/lib/python3.14/site-packages/playwright/driver/package/.local-browsers/chromium-1228/chrome-linux64/chrome"
 
     with sync_playwright() as p:
 
         browser = p.chromium.launch(
             headless=True,
-            executable_path=browser_path,
             args=[
                 "--no-sandbox",
                 "--disable-dev-shm-usage"
             ]
         )
 
-        page = browser.new_page()
+        page = browser.new_page(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
+        )
 
+
+        # открываем сайт для получения cookies
         page.goto(
-            URL,
-            wait_until="networkidle",
+            "https://rabotavdodo.ru/Lukhovitsy/eec89bd2a31db46311eedc5e2260fa4f/4254ceb0edc5826d11eff2ad492b901d",
+            wait_until="domcontentloaded",
             timeout=60000
         )
 
-        time.sleep(5)
+        time.sleep(10)
 
-        print("STATUS:", page.title())
-        print("URL:", page.url)
 
-        print(page.locator("body").inner_text()[:3000])
+        cookies = page.context.cookies()
+
+        print("COOKIES:")
+        print(cookies)
+
+
+        session = requests.Session()
+
+        for c in cookies:
+            session.cookies.set(
+                c["name"],
+                c["value"],
+                domain=c["domain"]
+            )
+
+
+        headers = {
+            "User-Agent": page.evaluate("navigator.userAgent"),
+            "Referer": "https://rabotavdodo.ru/"
+        }
+
+
+        r = session.get(
+            URL,
+            headers=headers
+        )
+
+
+        print("STATUS:", r.status_code)
+        print(r.text[:3000])
+
 
         browser.close()
+
 
 
 if __name__ == "__main__":

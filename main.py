985b1f26ccddfb6c6b8df70406f7a1d401cf9ca0
BirPechkin <gud2084@gmail.com>
2026-07-08 11:08:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from playwright.sync_api import sync_playwright
 url = "https://rabotavdodo.ru/api/dodois/vacancies?localities=ca2ee3d72c08bfa111efbd700111a8ac&staffTypes=Courier"
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
+    browser = p.chromium.launch(
+        headless=True,
+        executable_path="/opt/render/.cache/ms-playwright/chromium_headless_shell-1228/chrome-headless-shell-linux64/chrome-headless-shell"
+    )
+
     page = browser.new_page(
         user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/146 Safari/537.36"
     )
@@ -12,7 +16,6 @@ with sync_playwright() as p:
 
     print("STATUS:", response.status)
 
-    text = page.content()
-    print(text[:2000])
+    print(page.content()[:2000])
 
     browser.close()

4346479e367c87dac153def2e276629a8fdd089e
BirPechkin <gud2084@gmail.com>
2026-07-08 10:42:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-import requests
+from playwright.sync_api import sync_playwright
 
-URL = "https://rabotavdodo.ru/api/dodois/vacancies?localities=ca2ee3d72c08bfa111efbd700111a8ac&staffTypes=Courier"
+url = "https://rabotavdodo.ru/api/dodois/vacancies?localities=ca2ee3d72c08bfa111efbd700111a8ac&staffTypes=Courier"
 
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Referer": "https://rabotavdodo.ru/Zaraysk?tabSlug=delivery",
-    "Accept": "application/json"
-}
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page(
+        user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/146 Safari/537.36"
+    )
 
-response = requests.get(URL, headers=headers)
+    response = page.goto(url)
 
-print("STATUS:", response.status_code)
-print(response.text[:2000])
+    print("STATUS:", response.status)
+
+    text = page.content()
+    print(text[:2000])
+
+    browser.close()
